d97aa304d5ee03840d187833e8d70c019fd36823
Jazmin <blumi1702@googlemail.com>
2018-11-12 22:03:13 +0100

    try imports

---
+++ Pipeline.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+# Import all necessary modules 
+
+import warnings # Supress unnecessary warnings so that presentation looks clean
+warnings.filterwarnings('ignore')
+
+
+
+# In[6]:
+
+
+print("this"
+      )
+
+
+# In[3]:
+
+
+
+
+
+# In[ ]:
+
+
+# Run Data Exploration
+import testmodule
+from testmodule import firstFunction
+
+firstFunction()
+
+
+# In[5]:
+
+
+# Run Data Preprocessing and Feature Generation
+
+
+# In[4]:
+
+
+# Model Selection
+
+
+# In[5]:
+
+
+# Model Training
+
+
+# In[6]:
+
+
+# Model Evaluation
+
+
+# In[7]:
+
+
+# Model Interpretation
+
