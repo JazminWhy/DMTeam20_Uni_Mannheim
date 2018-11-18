3b5297591ce04f62bbdadb79fdb828a5e44e1c67
Marius Bock <marius.bock@protonmail.com>
2018-11-18 20:25:37 +0100

    ModelTraining XGBoost + LGBM

--- modelTrainingMarius.py
+++ modelTrainingMarius.py
@@ -1,8 +1,7 @@
-import xgboost as xgb
-from xgboost import XGBClassifier
-import lightgbm as lgb
-from lightgbm import LGBMClassifier
+import pandas as pd
 import numpy as np
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import StratifiedKFold, KFold
 
@@ -75,7 +74,7 @@ def train_lgbm_model(x_train, y_train, params, n_folds, early_stopping, random_s
     # return model for evaluation and prediction
     return lgbm_model
 
-def train_xgb_model(params, fit_params,x_train, y_train, n_folds, random_state, stratified = True, i=0, shuffle = True):
+def train_xgb_model(x_train, y_train, params, n_folds, random_state, stratified = True, shuffle = True):
 
     # Model and hyperparameter selection
     if stratified:
@@ -84,6 +83,7 @@ def train_xgb_model(params, fit_params,x_train, y_train, n_folds, random_state, 
         kf = KFold(n_splits=n_folds, random_state=random_state, shuffle=shuffle)
 
     xgb_model = XGBClassifier(**params)
+    i = 0
 
     # Model Training
     for (train_index, test_index) in kf.split(x_train, y_train):
@@ -94,7 +94,7 @@ def train_xgb_model(params, fit_params,x_train, y_train, n_folds, random_state, 
         y_train_cv, y_val_cv = y_train.iloc[train_index], y_train.iloc[test_index]
 
         # declare your model
-        xgb_model.fit(x_train_cv, y_train_cv, fit_params, eval_set=[(x_train_cv, y_train_cv), (x_val_cv, y_val_cv)])
+        xgb_model.fit(x_train_cv, y_train_cv, early_stopping_rounds=50, eval_set=[(x_train_cv, y_train_cv), (x_val_cv, y_val_cv)])
 
         # predict train and validation set accuracy and get eval metrics
         scores_cv = xgb_model.predict(x_train_cv)
@@ -121,6 +121,57 @@ def train_xgb_model(params, fit_params,x_train, y_train, n_folds, random_state, 
     # return model for evaluation and prediction
     return xgb_model
 
+
+def train_ensemble(models, y_train, ensemble_algorithm, params, fit_params, n_folds, random_state, stratified = True, shuffle = True):
+    # Model and hyperparameter selection
+    if stratified:
+        kf = StratifiedKFold(n_splits=n_folds, random_state=random_state, shuffle=shuffle)
+    else:
+        kf = KFold(n_splits=n_folds, random_state=random_state, shuffle=shuffle)
+
+    ensemble_model = ensemble_algorithm(**params)
+    i = 0
+    j = 0
+
+    x_train = pd.Dataframe()
+
+    for model in models:
+        x_train['model_'&j] = model.predict_proba(y_train)
+        j = j + 1
+
+    # Model Training
+    for (train_index, test_index) in kf.split(x_train, y_train):
+        # cross-validation randomly splits train data into train and validation data
+        print('\n Fold %d' % (i + 1))
+
+        x_train_cv, x_val_cv = x_train.iloc[train_index], x_train.iloc[test_index]
+        y_train_cv, y_val_cv = y_train.iloc[train_index], y_train.iloc[test_index]
+
+        # declare your model
+        ensemble_model.fit(x_train_cv, y_train_cv, fit_params,
+                      eval_set=[(x_train_cv, y_train_cv), (x_val_cv, y_val_cv)])
+
+        # predict train and validation set accuracy and get eval metrics
+        scores_cv = ensemble_model.predict(x_train_cv)
+        scores_val = ensemble_model.predict(x_val_cv)
+
+        # training evaluation
+
+        train_pc = accuracy_score(y_train_cv, scores_cv)
+        train_pp = precision_score(y_train_cv, scores_cv)
+        train_re = recall_score(y_train_cv, scores_cv)
+        print('\n train-Accuracy: %.6f' % train_pc)
+        print(' train-Precision: %.6f' % train_pp)
+        print(' train-Recall: %.6f' % train_re)
+
+        eval_pc = accuracy_score(y_val_cv, scores_val)
+        eval_pp = precision_score(y_val_cv, scores_val)
+        eval_re = recall_score(y_val_cv, scores_val)
+        print('\n eval-Accuracy: %.6f' % eval_pc)
+        print(' eval-Precision: %.6f' % eval_pp)
+        print(' eval-Recall: %.6f' % eval_re)
+
+        i = i + 1
 
 
 
